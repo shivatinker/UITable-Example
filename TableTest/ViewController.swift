30cf37bb7997f2377184ca95d0490f00c430246a
negativex <negativex228@gmail.com>
2020-03-19 17:46:43 +0200

    Initial Commit

---
+++ TableTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableTest
+//
+//  Created by Andrew Zinoviev on 3/19/20.
+//  Copyright © 2020 Andrew Zinoviev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
