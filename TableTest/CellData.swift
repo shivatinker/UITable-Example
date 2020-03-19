bb704b4853261bae402e103295a99a954910c575
negativex <negativex228@gmail.com>
2020-03-20 00:39:30 +0200

    v 2

---
+++ TableTest/CellData.swift
@@ -0,0 +1,20 @@
+//
+//  CellData.swift
+//  TableTest
+//
+//  Created by Andrew Zinoviev on 3/19/20.
+//  Copyright © 2020 Andrew Zinoviev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CellData {
+    var name: String
+    var color: UIColor
+    
+    init(name:String, color:UIColor) {
+        self.name = name
+        self.color = color
+    }
+}
--- TableTest/MyTableViewController.swift
+++ TableTest/MyTableViewController.swift
@@ -28,19 +28,13 @@ class MyTableViewController: UITableViewController {
         loadCells()
     }
     
-    //    override func prepareForInterfaceBuilder() {
-    //        self.tableView.register(UINib(nibName: "MyCell", bundle: Bundle.main), forCellReuseIdentifier: "MyCellId")
-    //    }
-    
     // MARK: - Table view data source
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        
         return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
         return cells.count
     }
     
@@ -49,17 +43,48 @@ class MyTableViewController: UITableViewController {
     }
     
     private func loadCells(){
-        if let cell = CellData(name: "Cell1", color: hueColor(hue: 0.1)){
-            cells.append(cell)
+        let session = URLSession(configuration: .default)
+        var dataTask: URLSessionDataTask?
+        self.cells.removeAll()
+        
+        dataTask?.cancel()
+        
+        guard let url = URLComponents(string: "https://api.myjson.com/bins/vi56v")?.url else {
+            return
         }
-        if let cell = CellData(name: "Cell2", color: hueColor(hue: 0.4)){
-            cells.append(cell)
-        }
-        if let cell = CellData(name: "Cell3", color: hueColor(hue: 0.8)){
-            cells.append(cell)
-        }
+        
+        dataTask = session.dataTask(with: url, completionHandler: { [weak self] data, response, error in
+            defer {
+                dataTask = nil
+            }
+            
+            if let error = error {
+                print(error.localizedDescription)
+            } else if
+                let data = data,
+                let response = response as? HTTPURLResponse,
+                response.statusCode == 200{
+                do{
+                    let res = try JSONDecoder().decode(JSONInput.self, from: data)
+                    print(res.companies)
+                    for s in res.companies{
+                        self?.cells.append(CellData(name: s, color: UIColor.black))
+                    }
+                    DispatchQueue.main.async {
+                        self?.tableView.reloadData()
+                    }
+                }catch let error {
+                    print(error)
+                }
+            }
+        })
+        dataTask?.resume()
     }
     
+    
+    class JSONInput: Codable{
+        let companies: Array<String>
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellId = "MyCellId"
@@ -73,50 +98,24 @@ class MyTableViewController: UITableViewController {
         return cell
     }
     
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let share = UIContextualAction(style: .normal, title: "Share", handler: {(action,view,cbk) in
+            let text = self.cells[indexPath.row].name
+            self.present(UIActivityViewController(activityItems: [text], applicationActivities: []), animated: true)
+            cbk(true)
+        })
+        share.backgroundColor = .lightGray
+
+        let delete = UIContextualAction(style: .destructive, title: "Delete", handler: {(action,view,cbk) in
+            self.tableView.beginUpdates()
+            self.cells.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+            self.tableView.endUpdates()
+            cbk(true)
+        })
+        delete.backgroundColor = .red
+        
+        return UISwipeActionsConfiguration(actions: [delete, share])
+    }
     
 }
